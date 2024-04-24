6fdf3a04011d273c3c52ac0426c10d1c970c9ff5
Jessica Donahue <jmd732@nyu.edu>
2024-04-24 10:17:49 -0400

    Add session and signin callbacks in nextauth handler to connect to mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,39 +10,38 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    // callbacks: {
-    //     async session({ session }) {
-    //         console.log('session callback', session)
-    //         const sessionUser = await User.findOne({
-    //             email: session.user.email
-    //         });
-    //         console.log(sessionUser)
-    //         session.user.id = sessionUser._id.toString();
-    //     },
-    //     async signIn({ profile }) {
-    //         console.log('signIn callback')
-    //         try {
-    //             await connectToDB();
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            });
+            console.log(sessionUser)
+            session.user.id = sessionUser._id.toString();
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
     
-    //             // check if a user already exists
-    //             const userExists = await User.findOne({
-    //                 email: profile.email
-    //             })
-    //             // if not, create a new user and save to db
-    //             if(!userExists) {
-    //                 await User.create({
-    //                     email: profile.email,
-    //                     username: profile.name.replace(' ', '').toLowerCase(),
-    //                     image: profile.picture
-    //                 })
-    //             }
-    //             return true;
-    //         } catch (error) {
-    //             console.log(error)
-    //             return false;
-    //         }
-    //     }
-    // }
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
+                // if not, create a new user and save to db
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(' ', '').toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false;
+            }
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
